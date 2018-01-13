a0a18d1a40dfa14f5fcca8c9429eb356a8c8f272
Michelle Killian <michellekillian723@gmail.com>
2018-01-13 15:45:07 -0600

    get app ready to publish

--- project.clj
+++ project.clj
@@ -1,13 +1,27 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "This is now fixed"
+  :description "clojure web app for displaying messages"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [hiccup "1.0.5"]
+                 [hickory "0.5.4"]
+                 [environ "1.0.0"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-environ "1.0.0"]]
   :ring {:handler chatter.handler/app}
+          :init chatter.handler/init
+          :destroy chatter/handler/destroy
+  :aot :all
+  :main chatter.handler
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  {:dev
+    {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}}
+  :production
+  {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}
+    :env {production true}}
+  :uberjar-name "chatter-standalone.jar")
--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -3,10 +3,19 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
-            [hiccup.form :as form]))
+            [hiccup.form :as form]
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
 
 (def chat-messages (atom '()))
+
+(defn init []
+  (println "chatter is starting"))
+
+(defn destroy []
+  (println "chatter is shutting down"))
 
 (defn update-messages!
   "This will update a message list atom"
@@ -18,7 +27,10 @@
     [messages]
       (page/html5
         [:head
-          [:title "chatter"]]
+          [:title "chatter"]
+          (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+          (page/include-js "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+          (page/include-css "/chatter.css")]
         [:body
           [:h1 "Our Chat App"]
           [:p
@@ -28,7 +40,7 @@
             "Message: " (form/text-field "msg")
             (form/submit-button "Submit"))]
           [:p
-            [:table
+            [:table#messages.table.table-bordered.table-hover
               (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
   (defroutes app-routes
     (GET "/" [] (generate-message-view @chat-messages))
@@ -38,6 +50,11 @@
               new-messages (update-messages! chat-messages name-param msg-param)]
               (generate-message-view new-messages)
               ))
+    (route/resources "/")
     (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
