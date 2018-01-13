2e9816bbb156882a2c4036611ce8504a0a86dd59
Michelle Killian <michellekillian723@gmail.com>
2018-01-13 13:09:13 -0600

    fixing README.md description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "This is now fixed"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
